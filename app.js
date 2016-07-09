d598e9ffc1b462e08fa160666f2d6ad6b3fbd237
noppelmax <noppelmax@googlemail.com>
2016-07-09 18:20:16 +0200

    implement doortstate

---
+++ app.js
@@ -0,0 +1,21 @@
+
+function updateDoorstate(){
+
+  console.log("Updating doorstate!");
+  $.get("http://vspace.one/spaceapi.json", function(data, textStatus){
+    if(data.state.open == 1){
+      $("#doorstate").addClass("label-success");
+      $("#doorstate").removeClass("label-danger");
+      $("#doorstate").text("opened");
+
+    }else{
+      $("#doorstate").addClass("label-danger");
+      $("#doorstate").removeClass("label-success");
+      $("#doorstate").text("closed");
+
+    }
+
+
+
+  });
+}
