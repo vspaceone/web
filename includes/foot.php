a97ccfabc59405a9377ebf6c76d68065f2187428
noppelmax <gitkraken@noppelmax.online>
2018-10-02 14:03:39 +0200

    Fix footer links

--- includes/foot.php
+++ includes/foot.php
@@ -1,3 +1,3 @@
 <!-- Footer -->
-<footer class="container-fluid bg-4 text-center"><p><a href="impressum.php" style="color: #ffffff">Impressum</a> | <a href="datenschutzerklaerung.php" style="color: #ffffff">Datenschutzerklärung</a> | <a href="dashboard.php" style="color: #ffffff">Dashboard</a> | <a href="downloads.php" style="color: #ffffff">Downloads</a> | <a href="mitgliedwerden.php" style="color: #ffffff">Werde Mitglied!</a> </p>
+<footer class="container-fluid bg-4 text-center"><p><a href="?page=impressum.php" style="color: #ffffff">Impressum</a> | <a href="?page=datenschutzerklaerung.php" style="color: #ffffff">Datenschutzerklärung</a> | <a href="?page=dashboard.php" style="color: #ffffff">Dashboard</a> | <a href="?page=downloads.php" style="color: #ffffff">Downloads</a> | <a href="?page=mitgliedwerden.php" style="color: #ffffff">Werde Mitglied!</a> </p>
 </footer>
