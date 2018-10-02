6517e437d0a1fbdd45f3b9335878911c2cc1ace4
noppelmax <gitkraken@noppelmax.online>
2018-10-02 14:19:51 +0200

    sta size for doorstate label

--- includes/nav.php
+++ includes/nav.php
@@ -1,4 +1,11 @@
 <!-- Navbar -->
+<style>
+.doorstatelabel {
+  min-width: 200px !important;
+  display: inline-block !important
+}
+</style>
+
 <nav class="navbar navbar-default">
     <div class="container">
         <div class="navbar-header">
@@ -18,7 +25,7 @@
                 <li><a href="https://wiki.vspace.one">Wiki</a></li>
                 <li><a class="faw-link" href="https://t.me/joinchat/DmNdswpnKgoa2KIPjZfxZA"><i class="fab fa-telegram"></i></a></li>
                 <li>
-                    <div id="doorstate" class="label label-success invisible"><a href="#state" id="doorstateString">loading</a></div>
+                    <div id="doorstate" class="label label-success invisible doorstatelabel"><a href="#state" id="doorstateString">loading</a></div>
                 </li>
             </ul>
         </div>
