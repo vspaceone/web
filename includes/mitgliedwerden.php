8bb936419d5f3a3feb6479e6275efa3794005218
noppelmax <gitkraken@noppelmax.online>
2019-03-09 23:49:04 +0100

    Text verbessert/koorigiert

--- includes/mitgliedwerden.php
+++ includes/mitgliedwerden.php
@@ -5,8 +5,7 @@
     <div class="row">
         <div style="text-align:left;" class="col-sm-3"></div>
         <div style="text-align:left;" class="col-sm-6">
-            Lade dir unter <a href="?page=downloads">Downloads</a> die Beitrittserklärung herunter, bring sie mit in den Space und drücke sie einem der Vorstände in die Hände. Dafür eignet sich beispielsweise das zweiwöchig stattfindende Plenum
-            am Dienstagabend. Alternativ schicke sie an folgende Adresse:<br><br> vspace.one e.V <br> Wilhelm-Binder-Str. 19 <br> 78048 Villingen
+            Lade dir unter <a href="?page=downloads">Downloads</a> die Beitrittserklärung herunter, bringe sie mit in den Space und drücke sie einem der Vorstände in die Hände. Dafür eignet sich der wöchentliche Chaostreff am Dienstagabend. Alternativ schicke sie an folgende Adresse:<br><br> vspace.one e.V <br> Wilhelm-Binder-Str. 19 <br> 78048 Villingen-Schwenningen
         </div>
         <div style="text-align:left;" class="col-sm-3"></div>
     </div>
